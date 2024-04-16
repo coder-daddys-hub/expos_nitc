921b8eb399c308a28bc504e97cd7fe5d621f8c8f
firstname surname <me@hotmail.com>
2024-04-16 15:59:07 +0000

    modify 'making things work', remove sample.dat creation, add sample.dat file

--- scripts/makingThingsWork_Stage2.sh
+++ scripts/makingThingsWork_Stage2.sh
@@ -6,11 +6,6 @@ cd $HOME/myexpos/xfs-interface
 ./xfs-interface fdisk
 ./xfs-interface df
 
-read -p "press any key to continue"
-
-mkdir $HOME/myexpos/assignments
-echo "There is a place where the sidewalk ends And before the street begins, And there the grass grows soft and white, And there the sun burns crimson bright,And there the moon-bird rests from his flight To cool in the peppermint wind." > $HOME/myexpos/assignments/sample.dat
-
 mkdir $HOME/myexpos/xfs-interface/xsm_extractions
 
 cd $HOME/myexpos/xfs-interface
