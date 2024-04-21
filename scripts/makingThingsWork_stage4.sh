19092d0730c2211bb0d672ff47af3c78ce88c99e
firstname surname <me@hotmail.com>
2024-04-21 19:07:01 +0000

    correction to making things work: add compilation for user program

--- scripts/makingThingsWork_stage4.sh
+++ scripts/makingThingsWork_stage4.sh
@@ -2,5 +2,8 @@
 
 ## STAGE 4 SPL Language
 
+cd $HOME/myexpos/spl
+./spl spl_progs/user_programs/oddnos.spl
+
 cd $HOME/myexpos/xfs-interface
-./xfs-interface load --os $HOME/myexpos/spl/spl_progs/oddnos.spl
+./xfs-interface load --os $HOME/myexpos/spl/spl_progs/oddnos.xsm#!/bin/bash
